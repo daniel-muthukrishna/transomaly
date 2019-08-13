f5449800c49378f38d8ef9334403a5cf025b188e
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2019-08-13 16:23:13 -0700

    Moved get_data to a new file "get_training_data.py".

--- transomaly/fit_gaussian_processes.py
+++ transomaly/fit_gaussian_processes.py
@@ -9,36 +9,7 @@ from scipy.optimize import minimize
 import matplotlib
 matplotlib.use('Agg')
 
-from transomaly.read_light_curves_from_snana_fits import read_light_curves_from_snana_fits_files
-
-
-def get_data(class_num, data_dir='data/ZTF_20190512/', save_dir='data/saved_light_curves/', passbands=('g', 'r'),
-             nprocesses=1, redo=False):
-    save_lc_filepath = os.path.join(save_dir, f"lc_classnum_{class_num}.pickle")
-
-    if os.path.exists(save_lc_filepath) and not redo:
-        with open(save_lc_filepath, "rb") as fp:  # Unpickling
-            light_curves = pickle.load(fp)
-    else:
-        class_dir = os.path.join(data_dir, 'ZTF_MSIP_MODEL{:02d}'.format(class_num))
-        files = os.listdir(class_dir)
-
-        head_files = []
-        phot_files = []
-        for file in files:
-            filepath = os.path.join(data_dir, class_dir, file)
-            if filepath.endswith('HEAD.FITS'):
-                head_files.append(filepath)
-                phot_files.append(filepath.replace('_HEAD.FITS', '_PHOT.FITS'))
-            print(filepath)
-
-        light_curves = read_light_curves_from_snana_fits_files(head_files, phot_files, passbands, known_redshift=False, nprocesses=nprocesses)
-
-        with open(save_lc_filepath, "wb") as fp:  # Pickling
-            pickle.dump(light_curves, fp)
-
-    return light_curves
-
+from transomaly.get_training_data import get_data
 
 def combined_neg_log_like(params, fluxes, gp_lcs, passbands):
     loglike = 0
@@ -112,8 +83,6 @@ def fit_gaussian_process(args):
             plt.plot(x, pred_mean, color=color[pb])
             plt.fill_between(x, pred_mean + pred_std, pred_mean - pred_std, color=color[pb], alpha=0.3,
                              edgecolor="none")
-
-    # TODO: share hyperparameters across passband light curves
 
     if plot:
         plt.xlabel("Days since trigger")
