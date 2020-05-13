Added option for "bayesian" network in both the TCN class and the network. Need to fix Convolutional1D not using 'causal' padding. Also need to fix disabling eager_execution required because of known issue with Conv1DFlipout in current tf 2.2 and tfp 0.9 versions. Problem identified here: https://github.com/tensorflow/probability/issues/620

tcn_variational.py
@@ -5,16 +5,16 @@ which is an implementation of the Temporal Convolutional Network described in
 Bai, Kolter and Kiltun (2018) https://arxiv.org/pdf/1803.01271.pdf
 
 Changes made:
-12-May-2020: Added lines
-    from tensorflow.keras.layers import Layer, BatchNormalization, LayerNormalization # Conv1D, Dense
-    import tensorflow_probability as tfp
-    Conv1D = tfp.layers.Convolution1DFlipout
-    Dense = tfp.layers.DenseFlipout
 
-    and commented out references to kernel_initializer in Conv1D since Convolution1DFlipout does not use this
-
-13-May-2020: Problem with Convolutional1DFlipout, Tensorflow 2.2 and Tensorflow_probability 0.9.
+13-May-2020:
+    Problem with Convolutional1DFlipout, Tensorflow 2.2 and Tensorflow_probability 0.9.
     known issue here: https://github.com/tensorflow/probability/issues/620
+
+13-May-2020:
+    Added option for bayesian network by replacing tf.keras.layers.Conv1D with tfp.layers.Convolution1DFlipout
+    and commented out references to kernel_initializer in Convolution1DFlipout.
+    Need to fix issue with Convolutional1DFlipout not having padding='causal' as an option
+
 """
 
 import inspect
@@ -23,11 +23,10 @@ from typing import List
 from tensorflow.keras import backend as K, Model, Input, optimizers
 from tensorflow.keras import layers
 from tensorflow.keras.layers import Activation, SpatialDropout1D, Lambda
-from tensorflow.keras.layers import Layer, BatchNormalization, LayerNormalization, Conv1D, Dense
+from tensorflow.keras.layers import Layer, BatchNormalization, LayerNormalization
 
-# import tensorflow_probability as tfp
-# Conv1D = tfp.layers.Convolution1DFlipout
-# Dense = tfp.layers.DenseFlipout
+import tensorflow as tf
+import tensorflow_probability as tfp
 
 
 def is_power_of_two(num):
@@ -52,6 +51,7 @@ class ResidualBlock(Layer):
                  activation='relu',
                  dropout_rate=0,
                  kernel_initializer='he_normal',
+                 bayesian=False,
                  use_batch_norm=False,
                  use_layer_norm=False,
                  **kwargs):
@@ -82,6 +82,7 @@ class ResidualBlock(Layer):
         self.use_batch_norm = use_batch_norm
         self.use_layer_norm = use_layer_norm
         self.kernel_initializer = kernel_initializer
+        self.bayesian = bayesian
         self.layers = []
         self.layers_outputs = []
         self.shape_match_conv = None
@@ -109,12 +110,20 @@ class ResidualBlock(Layer):
             for k in range(2):
                 name = 'conv1D_{}'.format(k)
                 with K.name_scope(name):  # name scope used to make sure weights get unique names
-                    self._add_and_activate_layer(Conv1D(filters=self.nb_filters,
-                                                        kernel_size=self.kernel_size,
-                                                        dilation_rate=self.dilation_rate,
-                                                        padding=self.padding,
-                                                        name=name))
-                                                        # kernel_initializer=self.kernel_initializer))
+                    if self.bayesian:
+                        self._add_and_activate_layer(tfp.layers.Convolution1DFlipout(filters=self.nb_filters,
+                                                            kernel_size=self.kernel_size,
+                                                            dilation_rate=self.dilation_rate,
+                                                            padding=self.padding,
+                                                            name=name))
+                                                            # kernel_initializer=self.kernel_initializer))
+                    else:
+                        self._add_and_activate_layer(tf.keras.layers.Conv1D(filters=self.nb_filters,
+                                                            kernel_size=self.kernel_size,
+                                                            dilation_rate=self.dilation_rate,
+                                                            padding=self.padding,
+                                                            name=name,
+                                                            kernel_initializer=self.kernel_initializer))
 
                 with K.name_scope('norm_{}'.format(k)):
                     if self.use_batch_norm:
@@ -130,11 +139,18 @@ class ResidualBlock(Layer):
                 name = 'matching_conv1D'
                 with K.name_scope(name):
                     # make and build this layer separately because it directly uses input_shape
-                    self.shape_match_conv = Conv1D(filters=self.nb_filters,
-                                                   kernel_size=1,
-                                                   padding='same',
-                                                   name=name)
-                                                   # kernel_initializer=self.kernel_initializer)
+                    if self.bayesian:
+                        self.shape_match_conv = tfp.layers.Convolution1DFlipout(filters=self.nb_filters,
+                                                       kernel_size=1,
+                                                       padding='same',
+                                                       name=name)
+                                                       # kernel_initializer=self.kernel_initializer)
+                    else:
+                        self.shape_match_conv = tf.keras.layers.Conv1D(filters=self.nb_filters,
+                                                       kernel_size=1,
+                                                       padding='same',
+                                                       name=name,
+                                                       kernel_initializer=self.kernel_initializer)
 
             else:
                 name = 'matching_identity'
@@ -214,6 +230,7 @@ class TCN(Layer):
                  kernel_initializer='he_normal',
                  use_batch_norm=False,
                  use_layer_norm=False,
+                 bayesian=False,
                  **kwargs):
 
         self.return_sequences = return_sequences
@@ -234,6 +251,7 @@ class TCN(Layer):
         self.build_output_shape = None
         self.lambda_layer = None
         self.lambda_ouput_shape = None
+        self.bayesian = bayesian
 
         if padding != 'causal' and padding != 'same':
             raise ValueError("Only 'causal' or 'same' padding are compatible for this layer.")
@@ -276,6 +294,7 @@ class TCN(Layer):
                                                           use_batch_norm=self.use_batch_norm,
                                                           use_layer_norm=self.use_layer_norm,
                                                           kernel_initializer=self.kernel_initializer,
+                                                          bayesian=self.bayesian,
                                                           name='residual_block_{}'.format(len(self.residual_blocks))))
                 # build newest residual block
                 self.residual_blocks[-1].build(self.build_output_shape)
@@ -359,7 +378,8 @@ def compiled_tcn(num_feat,  # type: int
                  opt='adam',
                  lr=0.002,
                  use_batch_norm=False,
-                 use_layer_norm=False):
+                 use_layer_norm=False,
+                 bayesian=False):
     # type: (...) -> Model
     """Creates a compiled TCN model for a given task (i.e. regression or classification).
     Classification uses a sparse categorical loss. Please input class ids and not one-hot encodings.
@@ -408,7 +428,10 @@ def compiled_tcn(num_feat,  # type: int
 
     if not regression:
         # classification
-        x = Dense(num_classes)(x)
+        if bayesian:
+            x = tfp.layers.DenseFlipout(num_classes)(x)
+        else:
+            x = tf.keras.layers.Dense(num_classes)(x)
         x = Activation('softmax')(x)
         output_layer = x
         model = Model(input_layer, output_layer)
@@ -428,7 +451,10 @@ def compiled_tcn(num_feat,  # type: int
         model.compile(get_opt(), loss='sparse_categorical_crossentropy', metrics=[accuracy])
     else:
         # regression
-        x = Dense(output_len)(x)
+        if bayesian:
+            x = tfp.layers.DenseFlipout(output_len)(x)
+        else:
+            x = tf.keras.layers.Dense(output_len)(x)
         x = Activation('linear')(x)
         output_layer = x
         model = Model(input_layer, output_layer)

transomaly/train_network.py
@@ -20,7 +20,7 @@ from transomaly.plot_metrics import plot_metrics, plot_history
 
 def train_model(X_train, X_test, y_train, y_test, yerr_train, yerr_test, fig_dir='.', epochs=20, retrain=False,
                 passbands=('g', 'r'), model_change='', reframe=False, probabilistic=False, train_from_last_stop=0,
-                use_uncertainties=False):
+                use_uncertainties=False, bayesian=False):
 
     model_name = f"keras_model_epochs{epochs+train_from_last_stop}_{model_change}"
     model_filename = os.path.join(fig_dir, model_name, f"{model_name}.hdf5")
@@ -28,6 +28,9 @@ def train_model(X_train, X_test, y_train, y_test, yerr_train, yerr_test, fig_dir
         os.makedirs(os.path.join(fig_dir, model_name))
 
     npb = len(passbands)
+
+    if probabilistic and bayesian:
+        tf.compat.v1.disable_eager_execution()  # Needed because of known Tensorflow bug with Convolutional VI
 
     if probabilistic:
         if use_uncertainties:
@@ -58,7 +61,8 @@ def train_model(X_train, X_test, y_train, y_test, yerr_train, yerr_test, fig_dir
             model.add(Masking(mask_value=0., input_shape=(X_train.shape[1], X_train.shape[2])))
 
             model.add(TCN(100, return_sequences=True, kernel_size=2, nb_stacks=1, dilations=[1, 2, 4, 8],
-                          padding='causal', use_skip_connections=True, dropout_rate=0.0, activation='sigmoid'))
+                          padding='causal', use_skip_connections=True, dropout_rate=0.0, activation='sigmoid',
+                          bayesian=bayesian))
             # model.add(LSTM(100, return_sequences=True))
             # # model.add(Dropout(0.2, seed=42))
             # # model.add(BatchNormalization())
@@ -80,10 +84,11 @@ def train_model(X_train, X_test, y_train, y_test, yerr_train, yerr_test, fig_dir
                 # # model.add(BatchNormalization())
                 # # model.add(Dropout(0.2, seed=42))
                 if probabilistic:
-                    # every second column is the mean; every other column is the stddev
-                    model.add(TimeDistributed(Dense(npb*2)))
-                    # model.add(TimeDistributed(tfp.layers.DenseFlipout(npb * 2)))
-                    # model.add(TimeDistributed(tfp.layers.VariationalGaussianProcess(npb * 2)))
+                    if bayesian:
+                        # every second column is the mean; every other column is the stddev
+                        model.add(TimeDistributed(tfp.layers.DenseFlipout(npb * 2)))
+                    else:
+                        model.add(TimeDistributed(Dense(npb*2)))
                 else:
                     model.add(TimeDistributed(Dense(npb*1)))
 
@@ -136,8 +141,9 @@ def main():
     train_from_last_stop = 0
     normalise = True
     use_uncertainties = True
+    bayesian = True
 
-    nn_architecture_change = f"1TCN_{'probabilistic_' if probabilistic else ''}uncertainties{use_uncertainties}_predictfuture{npred}point_normalised{normalise}_nodropout_100units"
+    nn_architecture_change = f"1TCN_{'probabilistic_' if probabilistic else ''}bayesian{bayesian}_uncertainties{use_uncertainties}_predictfuture{npred}point_normalised{normalise}_nodropout_100units"
 
     fig_dir = os.path.join(fig_dir, "model_{}_ci{}_ns{}_c{}".format(otherchange, contextual_info, nsamples, class_nums))
     if not os.path.exists(fig_dir):
@@ -149,7 +155,7 @@ def main():
         preparearrays.make_training_set(class_nums, nsamples, otherchange, nprocesses, extrapolate_gp, reframe=reframe_problem, npred=npred, normalise=normalise, use_uncertainties=use_uncertainties)
 
     model, model_name = train_model(X_train, X_test, y_train, y_test, yerr_train, yerr_test, fig_dir=fig_dir, epochs=train_epochs,
-                        retrain=retrain, passbands=passbands, model_change=nn_architecture_change, reframe=reframe_problem, probabilistic=probabilistic, train_from_last_stop=train_from_last_stop, use_uncertainties=use_uncertainties)
+                        retrain=retrain, passbands=passbands, model_change=nn_architecture_change, reframe=reframe_problem, probabilistic=probabilistic, train_from_last_stop=train_from_last_stop, use_uncertainties=use_uncertainties, bayesian=bayesian)
 
     # plot_metrics(model, model_name, X_test, y_test, timesX_test, yerr_test, labels_test, objids_test, passbands=passbands,
     #              fig_dir=fig_dir, nsamples=nsamples, data_dir=data_dir, save_dir=save_dir, nprocesses=nprocesses, plot_gp=True, extrapolate_gp=extrapolate_gp, reframe=reframe_problem, plot_name='', npred=npred, probabilistic=probabilistic, known_redshift=known_redshift, get_data_func=get_data_func, normalise=normalise)